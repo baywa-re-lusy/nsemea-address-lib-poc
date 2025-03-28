aa4f62eb54034d2a443d95ef625d1be6fb9219e3
dariodds <dario.dasilva.private@gmail.com>
2025-03-28 10:07:15 +0100

    fixing postinstall v3

--- postInstall.js
+++ postInstall.js
@@ -24,7 +24,7 @@ fsExtra.copy(source,destination,{ preserveTimestamps:true}, err => {
 });
 
 console.log(`path : ${__dirname}`);
-const gitignorePath = path.resolve(__dirname, './.gitignore');
+const gitignorePath = path.resolve(__dirname, '/.gitignore');
 const entriesToAdd = ['src/Typescripts/Address', 'tests/__mock__/N'];
 
 console.log(`gitignorePath ${gitignorePath}`);
